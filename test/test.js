fabee9b7f2f37185b5010784a4952a4ebcd9f71f
ThomasR <thomasr@fantasymail.de>
2015-07-20 07:15:09 +0200

    More tests

--- test/test.js
+++ test/test.js
@@ -20,6 +20,7 @@ describe('JSON.sortify', function () {
                 false,
                 undefined,
                 'undefined',
+                '',
                 function bar() {},
                 /abc/
             ];
@@ -90,8 +91,11 @@ describe('JSON.sortify', function () {
                 [{a:{b:2, c:[{d:3}, 4, 'hello']}}, null],
                 [{a:{b:2, c:[{d:3}, 4, 'hello']}}, 1],
                 [{a:{b:2, c:[{d:3}, 4, 'hello']}}, 4],
+                [{a:{b:2, c:[{d:3}, 4, 'hello']}}, 11],
+                [{a:{b:2, c:[{d:3}, 4, 'hello']}}, -1],
                 [{a:{b:2, c:[{d:3}, 4, 'hello']}}, '\t'],
-                [{a:{b:2, c:[{d:3}, 4, 'hello']}}, 'gooble']
+                [{a:{b:2, c:[{d:3}, 4, 'hello']}}, 'gooble'],
+                [{a:{b:2, c:[{d:3}, 4, 'hello']}}, 'too long, must be shortened']
             ];
             fixtures.forEach(function (fixture) {
                 assert.equal(JSON.sortify(fixture[0], null, fixture[1]), JSON.stringify(fixture[0], null, fixture[1]));
@@ -137,6 +141,33 @@ describe('JSON.sortify', function () {
             fixtures.forEach(function (fixture) {
                 assert.equal(JSON.sortify(fixture[0]), fixture[1]);
             });
+        });
+
+        it('should sort keys and handle three arguments', function () {
+            var fixtures = [
+                [
+                    {x:1,b:2, a:3},
+                    ['a', 'b', 'c'],
+                    2,
+                    '{\n  "a": 3,\n  "b": 2\n}'
+                ],
+                [
+                    {x:1,b:{toJSON:function () {return 'b';}}, a:3},
+                    ['a', 'b', 'c'],
+                    '••',
+                    '{\n••"a": 3,\n••"b": "b"\n}'
+                ],
+                [
+                    {a:undefined,b:function(){},x:1,c:2,0:3,5:5,11:11,' d ':5,z:'foo',aa:'a',d:[{f:{h:2,e:1}},null,'2']},
+                    function (key,val) {return typeof val == 'string' ? val+'!!!' : val;},
+                    4,
+                    '{\n    "0": 3,\n    "5": 5,\n    "11": 11,\n    " d ": 5,\n    "aa": "a!!!",\n    "c": 2,\n    "d": [\n        {\n            "f": {\n                "e": 1,\n                "h": 2\n            }\n        },\n        null,\n        "2!!!"\n    ],\n    "x": 1,\n    "z": "foo!!!"\n}'
+                ]
+            ];
+            fixtures.forEach(function (fixture) {
+                assert.equal(JSON.sortify(fixture[0], fixture[1], fixture[2]), fixture[3]);
+            });
+
         });
     });
 });
